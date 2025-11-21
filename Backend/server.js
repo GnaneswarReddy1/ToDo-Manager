a09428bf02a3f1457f791bd5f5e9b92c6e6994dc
Gnaneswar Maram <gnaneswarreddy.187@gmail.com>
2025-11-20 22:03:02 -0500

    commit new server

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "https://to-do-manager-scak.vercel.app/login",
+  origin: "https://to-do-manager-scak.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
