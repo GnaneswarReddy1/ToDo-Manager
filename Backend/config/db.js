8b21b6d28965db0eb8bceffd19c2798b91794aa8
GnaneswarReddy1 <gnaneswarreddy.187@gmail.com>
2025-11-21 10:30:11 -0500

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -9,9 +9,9 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
-    console.log('✅ MongoDB connected');
+    console.log('MongoDB connected');
   } catch (err) {
-    console.error('❌ MongoDB connection error:', err);
+    console.error('MongoDB connection error:', err);
     process.exit(1); // Stop server if DB fails
   }
 };
